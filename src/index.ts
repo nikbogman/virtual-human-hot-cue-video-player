6ebb0e1ef8e0b3dcb8d8a7501a6b2616e4aea6e8
nikbogman <nicola.manoilov@gmail.com>
2026-05-31 15:10:58 +0200

    add broadcast prototype

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
+
+const app = new Hono()
+
+app.use('*', serveStatic({ root: './public' }))
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
